a62efa1618c92d6b1ba5790176dfe585336c98ac
SophieBonneau <sobonneau@aol.com>
2016-01-15 09:45:06 +0100

    Fire

--- src/main.cpp
+++ src/main.cpp
@@ -192,7 +192,6 @@ struct MyEventReceiver : IEventReceiver
         lefttail_node->setRotation(lefttailRotation);
         righttail_node->setRotation(righttailRotation);
 
-
         planeSpeedFloor = cos(childRotation.X * core::DEGTORAD) * planeSpeed;
         planeAltitude -= sin(childRotation.X * core::DEGTORAD) * planeSpeed;
 
@@ -284,22 +283,44 @@ struct MyEventReceiver : IEventReceiver
 void manageCollisionsWithScenery(is::ISceneManager *smgr,
                                  is::IMesh *city_mesh,
                                  is::ISceneNode* city_node,
-                                 is::IAnimatedMeshSceneNode *plane,
+                                 is::IMesh *airport_mesh,
+                                 is::ISceneNode* airport_node,
                                  is::ISceneNode *parentNode)
 {
-    // Création du triangle selector
+   bool collision = false;
+
+    //Collision with the landscape
     scene::ITriangleSelector *selector_city;
     selector_city = smgr->createOctreeTriangleSelector(city_mesh, city_node);
     city_node->setTriangleSelector(selector_city);
+    selector_city->drop();
     // Et l'animateur/collisionneur
     scene::ISceneNodeAnimator *anim_collision_plane_city;
     anim_collision_plane_city = smgr->createCollisionResponseAnimator(selector_city,
                                                  parentNode,  // Le noeud que l'on veut gérer
                                                  ic::vector3df(2.8, 0.5, 0.4), // "rayons" du perso
                                                  ic::vector3df(0, 0, 0),  // gravity
-                                                 ic::vector3df(1.0,0,0));  //décalage du centre
+                                                 ic::vector3df(0, 0, 0));  //décalage du centre
     parentNode->addAnimator(anim_collision_plane_city);
-    //smgr->addSphereSceneNode(5.0,16,0,-1,ic::vector3df(10,0,0),ic::vector3df(0,0,0),ic::vector3df(1.0,1.0,1.0));
+    anim_collision_plane_city->drop();
+
+    //Collision with the airport
+    scene::ITriangleSelector *selector_airport;
+    selector_airport = smgr->createOctreeTriangleSelector(airport_mesh, airport_node);
+    airport_node->setTriangleSelector(selector_airport);
+    selector_airport->drop();
+    // Et l'animateur/collisionneur
+    scene::ISceneNodeAnimator *anim_collision_plane_airport;
+    anim_collision_plane_airport = smgr->createCollisionResponseAnimator(selector_airport,
+                                                 parentNode,  // Le noeud que l'on veut gérer
+                                                 ic::vector3df(2.8, 0.5, 0.4), // "rayons" du perso
+                                                 ic::vector3df(0, 0, 0),  // gravity
+                                                 ic::vector3df(0, 0, 0));  //décalage du centre
+    parentNode->addAnimator(anim_collision_plane_airport);
+    anim_collision_plane_airport->drop();
+    parentNode->getTransformedBoundingBox().intersectsWithBox()
+
+    //mesh->getTransformedBoundingBox.intersectsWith(mesh2->getTransformedBoundingBox());
 }
 
 int main()
@@ -326,12 +347,26 @@ int main()
     GUIElements* guiManager = new GUIElements();
     guiManager->setDevice(device);
 
-    //City
+    device->getFileSystem()->addFileArchive("data.zip");
+
+    //Landscape and cities
     is::IMesh *city_mesh = smgr->getMesh("data/city/city_cercles.obj");
     is::ISceneNode *city_node;
     city_node = smgr->addOctreeSceneNode(city_mesh,nullptr,-1,1024);
     city_node->setMaterialFlag(iv::EMF_LIGHTING,false);
     city_node->setScale(ic::vector3df(10,10,10));
+
+    //Airport and runway
+    is::IMesh *airport_mesh = smgr->getMesh("data/airport/airport.obj");
+    is::ISceneNode *airport_node;
+    airport_node = smgr->addOctreeSceneNode(airport_mesh,nullptr,-1,1024);
+    airport_node->setMaterialFlag(iv::EMF_LIGHTING,false);
+    airport_node->setScale(ic::vector3df(10,10,10));
+    is::IMesh *runway_mesh = smgr->getMesh("data/airport/runway.obj");
+    is::ISceneNode *runway_node;
+    runway_node = smgr->addOctreeSceneNode(runway_mesh,nullptr,-1,1024);
+    runway_node->setMaterialFlag(iv::EMF_LIGHTING,false);
+    runway_node->setScale(ic::vector3df(10,10,10));
 
     //Init the object plane
     //2 parents: trajectory and rotation
@@ -402,11 +437,36 @@ int main()
     plan_water->setMaterialType(video::EMT_REFLECTION_2_LAYER);
     plan_water->setPosition(ic::vector3df(0,-2, 0));
 
+    //Fire
+    // create a particle system
+    scene::IParticleSystemSceneNode* ps = smgr->addParticleSystemSceneNode(false);
+    scene::IParticleEmitter* em = ps->createBoxEmitter(
+        core::aabbox3d<f32>(-0.7,0,-0.7,0.7,0.01,0.7), // emitter size
+        core::vector3df(0.0f,0.0006f,0.0f),   // initial direction
+        80000,100000,                             // emit rate
+        video::SColor(0,255,0,0),       // darkest color
+        video::SColor(0,255,255,255),       // brightest color
+        800,2000,0,                         // min and max age, angle
+        core::dimension2df(0.02f,0.02f),         // min size
+        core::dimension2df(0.04f,0.04f));        // max size
+    ps->setEmitter(em); // this grabs the emitter
+    em->drop(); // so we can drop it here without deleting it
+
+    scene::IParticleAffector* paf = ps->createFadeOutParticleAffector();
+    ps->addAffector(paf); // same goes for the affector
+    paf->drop();
+    ps->setPosition(core::vector3df(0,0,5));
+    ps->setScale(core::vector3df(0.05,0.05,0.05));
+    ps->setMaterialFlag(video::EMF_LIGHTING, false);
+    ps->setMaterialFlag(video::EMF_ZWRITE_ENABLE, false);
+    ps->setMaterialTexture(0, driver->getTexture("data/fire/fire.jpg"));
+    ps->setMaterialType(video::EMT_TRANSPARENT_ADD_COLOR);
+
     // 2D elements initialization
     guiManager->initialize2DElements();
 
     // Collision management with scenery
-    manageCollisionsWithScenery(smgr, city_mesh, city_node, plane_node, parentNode);
+    manageCollisionsWithScenery(smgr, city_mesh, city_node, airport_mesh, airport_node, parentNode);
 
     //Init the plane state
     //To change to false: true only for tests
